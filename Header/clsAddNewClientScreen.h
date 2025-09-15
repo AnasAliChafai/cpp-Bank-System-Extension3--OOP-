30ddfc469615cc91b84331fe473b56a1611b1a2e
Chafai Anas <anaschafai04@gmail.com>
2025-09-15 18:52:59 +0100

    Add files via upload

---
+++ Header/clsAddNewClientScreen.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "clsScreen.h"
+class clsAddNewClientScreen : public clsScreen
+{
+public :
+    static void ShowAddNewClient()
+    {
+        if (!CheckAccessRights(clsUser::enPermissions::pAddNewClient))
+        {
+            return;// this will exit the function and it will not continue
+        }
+        _DrawScreenHeader("\t  Add New Client Screen");
+        string AccountNumber = "";
+
+        cout << "\nPlease Enter Account Number: ";
+        AccountNumber = clsInputValidate::ReadString();
+        while (clsBankClient::IsClientExist(AccountNumber))
+        {
+            cout << "\nAccount Number Is Already Used, Choose another one: ";
+            AccountNumber = clsInputValidate::ReadString();
+        }
+
+        clsBankClient NewClient = clsBankClient::GetAddNewClientObject(AccountNumber);
+
+
+        _ReadClientInfo(NewClient);
+
+        clsBankClient::enSaveResults SaveResult;
+
+        SaveResult = NewClient.Save();
+
+        switch (SaveResult)
+        {
+        case  clsBankClient::enSaveResults::svSucceeded:
+        {
+            cout << "\nAccount Addeded Successfully :-)\n";
+            _PrintClient(NewClient);
+            break;
+        }
+        case clsBankClient::enSaveResults::svFaildEmptyObject:
+        {
+            cout << "\nError account was not saved because it's Empty";
+            break;
+
+        }
+        case clsBankClient::enSaveResults::svFaildAccountNumberExists:
+        {
+            cout << "\nError account was not saved because account number is used!\n";
+            break;
+
+        }
+        }
+    }
+};
+
