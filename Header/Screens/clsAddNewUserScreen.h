8b3414a316174144e7078905e271e5eb4c2d3152
Chafai Anas <anaschafai04@gmail.com>
2025-09-15 23:31:31 +0100

    Add files via upload

---
+++ Header/Screens/clsAddNewUserScreen.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "clsScreen.h"
+#include "clsUser.h"
+#include "clsInputValidate.h"
+
+class clsAddNewUserScreen : protected clsScreen
+{
+  
+public:
+
+    static void ShowAddNewUserScreen()
+    {
+
+        _DrawScreenHeader("\t  Add New User Screen");
+
+        string UserName = "";
+
+        cout << "\nPlease Enter UserName: ";
+        UserName = clsInputValidate::ReadString();
+        while (clsUser::IsUserExist(UserName))
+        {
+            cout << "\nUserName Is Already Used, Choose another one: ";
+            UserName = clsInputValidate::ReadString();
+        }
+
+        clsUser NewUser = clsUser::GetAddNewUserObject(UserName);
+
+        _ReadUserInfo(NewUser);
+
+        clsUser::enSaveResults SaveResult;
+
+        SaveResult = NewUser.Save();
+
+        switch (SaveResult)
+        {
+        case  clsUser::enSaveResults::svSucceeded:
+        {
+            cout << "\nUser Addeded Successfully :-)\n";
+            _PrintUser(NewUser);
+            break;
+        }
+        case clsUser::enSaveResults::svFaildEmptyObject:
+        {
+            cout << "\nError User was not saved because it's Empty";
+            break;
+
+        }
+        case clsUser::enSaveResults::svFaildUserExists:
+        {
+            cout << "\nError User was not saved because UserName is used!\n";
+            break;
+
+        }
+        }
+    }
+
+
+
+};
+
